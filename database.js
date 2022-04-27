eb2746f89bf09167b1f45c461f12aad7f6257a7f
iconicKuldeep <Kuldee.singh@iconicsoft.net>
2022-04-27 13:48:15 +0530

    env url

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
-const url = process.env.MONGODB_CONNECTION_URL_LOCATION_SERVICE;
+const url = process.env.MONGODB_CONNECTION_STRING;
 let _db;
 
 module.exports = {
